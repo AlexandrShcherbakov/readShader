be51a35ba208a29e3c8e911aa93b7a174d95020b
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2021-04-29 10:05:19 +0300

    Refactor and fix some components

--- readShader.py
+++ readShader.py
@@ -8,26 +8,32 @@ def _squash_brackets_expression(tokens):
             del tokens[i + 1]
         i -= 1
 
-_Token = collections.namedtuple("Token", ["var", "comp"])
+class _Token:
+    def __init__(self, var, comp):
+        self.var = var
+        self.comp = comp
+
+    def __str__(self):
+        return self.var + "." + self.comp
 
 def _uncombine_var_and_comp(tokens):
+    if '.' not in tokens[1]:
+        return
     res_token_comp = tokens[1].split('.')[1]
     if tokens[0].startswith("dp"):
         res_token_comp = "xyzw"[:int(tokens[0][2])]
-    for i in range(2, len(tokens)):
+    for i in range(1, len(tokens)):
         spl = tokens[i].split('.')
         if len(spl) != 2:
             continue
         tokens[i] = _Token(spl[0], spl[1])
-        tokens[i] = spl[0] + "." + spl[1]
-        if len(res_token_comp) == 1 and len(spl[1]) == 1:
+        if len(spl[1]) == 1 or i == 1:
             continue
         comp = ""
         comps = {"x": 0, "y": 1, "z": 2, "w": 3}
         for c in res_token_comp:
             comp += spl[1][comps[c]]
         tokens[i] = _Token(spl[0], comp)
-        tokens[i] = spl[0] + "." + comp
 
 
 def _process_const(tokens):
@@ -40,25 +46,13 @@ def _process_const(tokens):
         else:
             tokens[i] = "float" + str(components) + tokens[i][1:]
 
-
-class LdFormatter:
-    def __init__(self, reg_comp_count):
-        self.reg_comp_count = reg_comp_count
-
-    def format(self, *args):
-        buffer_var, buffer_comp = args[-1].split('.')
-        reg_var, reg_comp = args[0].split('.')
-        reg_comp = reg_comp[:self.reg_comp_count]
-        offset = args[1] if len(args) > 2 else "0"
-        return f"{buffer_var}[{reg_var}.{reg_comp} + {offset}].{buffer_comp}"
-
 def replace_dxil_tokens_with_hlsl(dxil):
     replace_table_operations = {
         "utof": "asfloat({})",
         "mad": "{} * {} + {}",
         "ftou": "asuint({})",
         "mov": "{}",
-        "ld_indexable(texture2darray)(float,float,float,float)": LdFormatter(3),
+        "ld_indexable(texture2darray)(float,float,float,float)": "{1.var}[{0}].{1.comp}",
         "ftoi": "asint({})",
         "ine": "{} != {}",
         "mul": "{} * {}",
@@ -66,7 +60,7 @@ def replace_dxil_tokens_with_hlsl(dxil):
         "movc": "{} ? {} : {}",
         "div": "{} / {}",
         "frc": "frac({})",
-        "ld_structured_indexable(structured_buffer,stride=16)(mixed,mixed,mixed,mixed)": LdFormatter(1),
+        "ld_structured_indexable(structured_buffer,stride=16)(mixed,mixed,mixed,mixed)": "{2.var}[{0} + {1}].{2.comp}",
         "and": "{} & {}",
         "add": "{} + {}",
         "dp3": "dot({}, {})",
@@ -75,7 +69,7 @@ def replace_dxil_tokens_with_hlsl(dxil):
     }
     replace_table_special = {
         "atomic_iadd": "InterlockedAdd({}[{}], {})",
-        "store_structured": "{}[{} + {}] = {}"
+        "store_structured": "{0.var}[{1} + {2}].{0.comp} = {3}"
     }
     replace_table_operators = {
         "if_nz": "if ({}) {{",
@@ -94,6 +88,7 @@ def replace_dxil_tokens_with_hlsl(dxil):
 
     hlsl = ""
     tabs = 0
+    tokens_lines = []
     for line in dxil.split('\n'):
         if not line.split(":")[0].strip().isdigit():
             hlsl += line + '\n'
@@ -102,12 +97,16 @@ def replace_dxil_tokens_with_hlsl(dxil):
         tokens = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(', '), command.split(' '))))
         _squash_brackets_expression(tokens)
         _process_const(tokens)
+        if tokens[0] in replace_table_operations or tokens[0] in replace_table_special:
+            _uncombine_var_and_comp(tokens)
+        tokens_lines.append(tokens)
+
+    for tokens in tokens_lines:
         loc_tabs = tabs
         if tokens[0] in local_tab_modifiers:
             loc_tabs += local_tab_modifiers[tokens[0]]
         if tokens[0] in replace_table_operations:
-            _uncombine_var_and_comp(tokens)
-            hlsl += "  " * loc_tabs + tokens[1] + " = " + replace_table_operations[tokens[0]].format(*tokens[2:]) + ";\n"
+            hlsl += "  " * loc_tabs + str(tokens[1]) + " = " + replace_table_operations[tokens[0]].format(*tokens[2:]) + ";\n"
         elif tokens[0] in replace_table_operators:
             hlsl += "  " * loc_tabs + replace_table_operators[tokens[0]].format(*tokens[1:]) + "\n"
         elif tokens[0] in replace_table_special:

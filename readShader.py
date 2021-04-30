492751722add77ea454e6e7458fba58c1d62b8ae
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2021-04-30 08:45:20 +0300

    Support variables with scopes

--- readShader.py
+++ readShader.py
@@ -16,6 +16,12 @@ class _Token:
     def __str__(self):
         return self.var + "." + self.comp
 
+    def __eq__(self, another):
+        return self.var == another.var and self.comp == another.comp
+
+    def __hash__(self):
+        return hash(f"{self.var}.{self.comp}")
+
 def _uncombine_var_and_comp(tokens):
     if '.' not in tokens[1]:
         return
@@ -49,6 +55,7 @@ def _process_const(tokens):
         else:
             tokens[i] = "float" + str(components) + tokens[i][1:] + ".xyzw"[:components + 1]
 
+
 class _Variable:
     def __init__(self, name, reg, comp):
         self.name = name
@@ -57,6 +64,10 @@ class _Variable:
 
     def __str__(self):
         return self.name
+    
+    def gen_reg_to_vars_pairs(self):
+        common_comps = "xyzw"[:len(self.comp)]
+        return {_Token(self.reg, self.comp[i]) : _Token(self.name, common_comps[i]) for i in range(len(common_comps))}
 
 def _token_to_single_regs(token):
     return [_Token(token.var, i) for i in token.comp]
@@ -65,6 +76,19 @@ def _replace_reg_with_var(reg, variables_map):
     for variable in variables_map.values():
         if reg.var == variable.reg and reg.comp in variable.comp:
             return _Token(variable.name, "xyzw"[variable.comp.index(reg.comp)])
+
+def _is_reg(token):
+    return isinstance(token, _Token) and token.var[0] == "r" and token.var[1:].isdigit()
+
+def _single_vars_to_vec(vars):
+    if len(vars) == 1:
+        return vars[0]
+    return f"float{len(vars)}({', '.join(list(map(str, vars)))})"
+
+def _find_var_in_scopes(scopes, reg):
+    for i in range(len(scopes) - 1, -1, -1):
+        if reg in scopes[i]:
+            return scopes[i][reg]
 
 def replace_dxil_tokens_with_hlsl(dxil):
     replace_table_operations = {
@@ -121,20 +145,29 @@ def replace_dxil_tokens_with_hlsl(dxil):
             _uncombine_var_and_comp(tokens)
         tokens_lines.append(tokens)
 
-    # variables_map = dict()
-    # variable_idx = 0
-    # for tokens in tokens_lines:
-    #     if tokens[0] in replace_table_operations:
-    #         for i in range(2, len(tokens)):
-    #             if isinstance(tokens[i], _Token) and tokens[i].var.startswith('r'):
-    #                 vars = [_replace_reg_with_var(reg, variables_map) for reg in _token_to_single_regs(tokens[i])]
-    #                 print(*vars)
-    #             if str(tokens[i]) in variables_map.keys():
-    #                 tokens[i] = variables_map[str(tokens[i])]
-    #         variable_name = f"v_{variable_idx}"
-    #         variable_idx += 1
-    #         variables_map[str(tokens[1])] = _Variable(variable_name, tokens[1].var, tokens[1].comp)
-    #         tokens[1] = _Variable(variable_name, tokens[1].var, tokens[1].comp)
+    variables_map = [dict()]
+    variable_idx = 0
+    for tokens in tokens_lines:
+        if tokens[0] in replace_table_operations:
+            for i in range(2, len(tokens)):
+                if not _is_reg(tokens[i]):
+                    continue
+                single_regs = _token_to_single_regs(tokens[i])
+                single_vars = [_find_var_in_scopes(variables_map, reg) for reg in single_regs]
+                tokens[i] = _single_vars_to_vec(single_vars)
+
+            variable_name = f"v_{variable_idx}"
+            variable_idx += 1
+            var = _Variable(variable_name, tokens[1].var, tokens[1].comp)
+            variables_map[-1].update(var.gen_reg_to_vars_pairs())
+            tokens[1] = var
+        if tokens[0].startswith("if"):
+            variables_map.append(dict())
+        elif tokens[0] == "else":
+            variables_map[-1] = dict()
+        elif tokens[0] == "endif":
+            variables_map = variables_map[:-1]
+                
 
     for tokens in tokens_lines:
         loc_tabs = tabs
